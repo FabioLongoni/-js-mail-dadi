4a0bb3625328c942518b22460afad3d01f1fd5a1
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-25 16:25:08 +0200

    end-bonus

--- bonus/js/bonus.js
+++ bonus/js/bonus.js
@@ -1,5 +1,5 @@
 
-const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 console.log(number);
 
 for ( let i = (number.length - 5); i < number.length; i++) {
