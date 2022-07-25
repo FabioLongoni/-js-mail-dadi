14b80b034e01291c7ca54a23236556d3d43679c4
FabioLongoni <longoni.fabio90@gmail.com>
2022-07-25 15:22:06 +0200

    end-Mail

--- Mail/js/app.js
+++ Mail/js/app.js
@@ -2,20 +2,24 @@
 let yourMail = prompt('whats your E-Mail?', 'E-Mail');
 console.log(yourMail);
 
-// let mailPresent = 'utente1@gmail.com';
 
 const mail = ['utente1@gmail.com','utente2@gmail.com','utente3@gmail.com'];
 console.log(mail)
 
+let mailValid
+
 for (let i = 0; i < mail.length; i++) {
-    
+    console.log(i);
     const mailRicercata = mail[i];
 
     if (mailRicercata === yourMail) {
-        console.log('your e-mail is valid');
-        
-    }else if (mailRicercata !== yourMail) {
-        console.log('not a valid e-mail');
+        mailValid = true;
     }
 
+}
+
+if (mailValid) {
+    console.log('your e-mail is valid');
+} else {
+    console.log('not a valid e-mail');
 }
